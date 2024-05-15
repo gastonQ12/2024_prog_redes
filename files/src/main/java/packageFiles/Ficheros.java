252a8b20659942e5ce9a578f6c6726dbb67c8b28
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-05-15 12:46:37 -0300

    Ficheros

--- files/src/main/java/packageFiles/Ficheros.java
+++ files/src/main/java/packageFiles/Ficheros.java
@@ -1,5 +1,30 @@
 package packageFiles;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 
 public class Ficheros {
 
+	private File archivo;
+	private PrintStream ps;
+	public Ficheros() {
+		
+    archivo = new File("c:\\User\\mario.txt");
+		
+	}
+	public void crearFilePrintStream(File a){
+		
+		FileOutputStream fos = null;
+		ps = new PrintStream(fos, true);
+		
+		ps.println("Hello World!");
+		ps.flush();
+		try {
+			fos = new FileOutputStream(a);
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+	}
 }
