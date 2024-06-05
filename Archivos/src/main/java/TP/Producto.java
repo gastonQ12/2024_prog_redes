2b8553f7bb738d26463a132bad57f50b044143f6
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-06-05 12:45:44 -0300

    subiendo primer codigo

---
+++ Archivos/src/main/java/TP/Producto.java
@@ -0,0 +1,19 @@
+package TP;
+
+public class Producto {
+
+	private String nombre;
+	private double precioCompra;
+	private double precioVenta;
+	private int stock;
+	Producto(String nombre, double precioCompra, double precioVenta, int stock) {
+		this.nombre = nombre;
+		this.precioCompra = precioCompra;
+		this.precioVenta = precioVenta;
+		this.stock = stock;
+	}
+	
+	
+	
+}
+
