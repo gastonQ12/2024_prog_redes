e58932b9be5be97293870d7d48e0f36128aabde0
gastonQ12 <162461760+gastonQ12@users.noreply.github.com>
2024-06-16 17:26:16 -0300

    Update
    si

--- TP1/src/main/java/TP1/main.java
+++ TP1/src/main/java/TP1/main.java
@@ -12,48 +12,59 @@ public class main {
 		Archivo resultados = new Archivo("C:\\archJava\\","resultados", ".txt");
 		Archivo error = new Archivo("C:\\archJava\\","error", ".txt");
 		Ficheros f = new Ficheros();
-		ArrayList<Double> array = new ArrayList(almacenarNumeros(f));
+		ArrayList<Integer> array = new ArrayList<Integer>(almacenarNumeros(f, error));
 		almacenarNumerosFile(f, a ,array);
 		cuenta(array, resultados, error, f);
 	}
 
 	
-	public static ArrayList<Double> almacenarNumeros(Ficheros f) {
-		ArrayList<Double> array = new ArrayList();
-		Double aux;
+	public static ArrayList<Integer> almacenarNumeros(Ficheros f, Archivo error) {
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		int aux;
 		for(int i=0; i<=4;i++) {
-			aux = Double.parseDouble(f.entradaDeDatos());
-			array.add(aux);}
-		for(int i=0; i<=4;i++){
-			f.ps.println(array.get(i));}
+			try{aux = Integer.parseInt(f.entradaDeDatos());
+				array.add(aux);
+			}
+			catch(NumberFormatException e) {
+				array.add(null);
+			}
+			}
+		//for(int i=0; i<=4;i++){
+			//f.ps.println(array.get(i));}
 		return array;
 	}
-	public static void almacenarNumerosFile(Ficheros f, Archivo a, ArrayList<Double> lista) {
+	public static void almacenarNumerosFile(Ficheros f, Archivo a, ArrayList<Integer> lista) {
 		for( double i = 0; i>=lista.size();i++){
 			int c = (int) i;
 			a.guardar(a.getArchivo(), String.valueOf(lista.get(c)));
 		}
 	}
-	public static void cuenta(ArrayList<Double> lista, Archivo resultados, Archivo error, Ficheros f) {
-		double aux2 = 0;
-		Iterator<Double> iter = lista.iterator();
-				for( double i = 0; i<=lista.size();i++) {
-				int c = (int) i;
-				try{aux2 = lista.get(c)/(lista.get(c+1)-3);}
-				 
-				catch (IndexOutOfBoundsException e) {
-		            error.guardar(error.getArchivo(), e.getMessage());
-				}
-				finally{
-					i=lista.size();
-				}
-				String str = String.valueOf(lista.get(c)).concat(" / ").concat(String.valueOf(lista.get(c+1)-3)).concat("=").concat(String.valueOf(aux2));
-				resultados.guardar(resultados.getArchivo(), str);
-				iter.next();
-			
-		}
-	     f.ps.print("falta un dato pibe");
-		
+	public static void cuenta(ArrayList<Integer> lista, Archivo resultados, Archivo error, Ficheros f) {
+			double aux = 0;
+			for (int i = 0; i < lista.size()- 1; i++) {
+	            try{
+	            	int num1 = lista.get(i);
+	            	int num2 = (lista.get(i+1)) - 3;
+	            	if(num2 == 0) {
+	            		
+	            		throw new ArithmeticException("no se puede dividir por 0");
+	            		
+	            	}
+	            	aux = (double) num1 / num2;
+	            	String str = String.valueOf(num1).concat(" / ").concat(String.valueOf(num2)).concat("=").concat(String.valueOf(aux));
+	            	resultados.guardar(resultados.getArchivo(), str);
+	            }
+	            catch(ArithmeticException e){
+				
+	            	error.guardar(error.getArchivo(), String.valueOf(e));
+	            }
+	            catch(NullPointerException e) {
+					error.guardar(error.getArchivo(), String.valueOf(e));
+	            }
+	            catch(NumberFormatException e) {
+					error.guardar(error.getArchivo(), String.valueOf(e));
+	            }
+		}		
 	}
 
 
